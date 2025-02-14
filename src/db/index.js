2e07e99f02318ae4f8adb95629a7f43b64d66d30
Rabby-khan-04 <ajrabbyk72@gmail.com>
2025-02-14 21:53:56 +0600

    set up initial server connection and database connection

---
+++ src/db/index.js
@@ -0,0 +1,25 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export const database = client.db("bistro-boss");
+
+async function connectDB() {
+  try {
+    // // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Successfully connected to MongoDB!!");
+  } catch (err) {
+    console.log(`MONGODB CONNECTION ERROR: ${err}`);
+  }
+}
+
+export default connectDB;
