877cdc50eb6afe25e63461bd1ee11f37ace6f8f4
Rabby-khan-04 <ajrabbyk72@gmail.com>
2025-02-21 02:41:30 +0600

    api for delete cart item added

--- src/controllers/cart.controllers.js
+++ src/controllers/cart.controllers.js
@@ -2,6 +2,7 @@ import status from "http-status";
 import { database } from "../db/index.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiResponse from "../utils/ApiResponse.js";
+import { ObjectId } from "mongodb";
 
 const cartCollection = database.collection("carts");
 
@@ -18,13 +19,43 @@ const getCart = asyncHandler(async (req, res) => {
   const { email } = req.query;
   const query = { email };
 
-  const cart = await cartCollection.find(query).toArray();
+  const cart = await cartCollection
+    .aggregate([
+      {
+        $match: query,
+      },
+      {
+        $lookup: {
+          from: "menu",
+          localField: "menuId",
+          foreignField: "_id",
+          as: "product",
+        },
+      },
+      {
+        $unwind: "$product",
+      },
+    ])
+    .toArray();
 
   return res
     .status(status.OK)
     .json(new ApiResponse(status.OK, cart, "Cart fetched successfully!!"));
 });
 
-const CartControllers = { addToCart, getCart };
+const deleteCartItem = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const query = { _id: new ObjectId(id) };
+
+  const result = await cartCollection.deleteOne(query);
+
+  return res
+    .status(status.OK)
+    .json(
+      new ApiResponse(status.OK, result, "Cart item deleted successfully!!")
+    );
+});
+
+const CartControllers = { addToCart, getCart, deleteCartItem };
 
 export default CartControllers;
--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.route("/add-to-cart").post(CartControllers.addToCart);
 router.route("/cart").get(CartControllers.getCart);
+router.route("/cart/:id").delete(CartControllers.deleteCartItem);
 
 export default router;
