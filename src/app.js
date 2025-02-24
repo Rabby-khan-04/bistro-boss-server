a9422fc05573b69b9ab69b777d49875fb15c426f
Rabby-khan-04 <ajrabbyk72@gmail.com>
2025-02-24 14:38:40 +0600

    fixed cors issue

--- src/app.js
+++ src/app.js
@@ -4,7 +4,13 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({ origin: process.env.CROSS_ORIGIN, credentials: true }));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN_PROD,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(cookieParser());
